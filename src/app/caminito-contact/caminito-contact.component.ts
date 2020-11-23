02d5cade92c9cf440ea3a0c940ff5d499df2fd29
fran <ffrann62@gmail.com>
2020-11-23 16:57:11 -0300

    se agrega contact para routing

---
+++ src/app/caminito-contact/caminito-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caminito-contact',
+  templateUrl: './caminito-contact.component.html',
+  styleUrls: ['./caminito-contact.component.scss']
+})
+export class CaminitoContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
